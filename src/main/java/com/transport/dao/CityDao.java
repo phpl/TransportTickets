59421a87589c965558ab350a824c0cc45d894ec7
Paweł Hadam <pawel.hadam19@gmail.com>
2017-12-31 01:24:14 +0100

    Change param name to more accurate

--- src/main/java/com/transport/dao/CityDao.java
+++ src/main/java/com/transport/dao/CityDao.java
@@ -13,13 +13,13 @@ import java.sql.SQLException;
 public class CityDao {
     private DatabaseService databaseService = null;
 
-    public void insertCity(CityEntity newCity) throws SQLException {
+    public void insertCity(CityEntity newEntity) throws SQLException {
         String insertNewCity = "INSERT INTO transport.miasto (nazwa) VALUES (?);";
 
         try (PreparedStatement preparedStatement = databaseService.getConnection().prepareStatement(insertNewCity)) {
             log.info("Begin insertNewCity");
 
-            preparedStatement.setString(1, newCity.getName());
+            preparedStatement.setString(1, newEntity.getName());
             preparedStatement.executeUpdate();
 
             log.info("End insertNewCity");
