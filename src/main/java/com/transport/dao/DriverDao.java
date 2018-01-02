a91d8a7bc88fb1f82027e9ad8d6ffbdb6d1599be
Paweł Hadam <pawel.hadam19@gmail.com>
2018-01-02 14:43:08 +0100

    Add insert

--- src/main/java/com/transport/dao/DriverDao.java
+++ src/main/java/com/transport/dao/DriverDao.java
@@ -1,12 +1,43 @@
 package com.transport.dao;
 
 import com.transport.DatabaseService;
+import com.transport.entity.DriverEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Log4j
 @RequiredArgsConstructor
 public class DriverDao {
     private DatabaseService databaseService = null;
 
+    private String insertNewDriver = "INSERT INTO " +
+            "transport.kierowca (imie, nazwisko, numer_telefonu, termin_waznosci_badan, termin_waznosci_prawa_jazdy) " +
+            "VALUES (?, ?, ?, ?, ?);";
+
+    public void insertDriver(DriverEntity newEntity) throws SQLException {
+        databaseService.setAutoCommit(false);
+        executeInsert(newEntity);
+        databaseService.setAutoCommit(true);
+    }
+
+    private void executeInsert(DriverEntity newEntity) {
+        try (PreparedStatement preparedStatement = databaseService.getConnection().prepareStatement(insertNewDriver)) {
+            log.info("Begin insertNewDriver");
+
+            preparedStatement.setString(1, newEntity.getFirstName());
+            preparedStatement.setString(2, newEntity.getLastName());
+            preparedStatement.setInt(1, newEntity.getPhoneNumber());
+            preparedStatement.setObject(1, newEntity.getPeriodOfMedicalCheckUpValidation());
+            preparedStatement.setObject(1, newEntity.getPeriodOfDriverLicenceValidation());
+            preparedStatement.executeUpdate();
+
+            log.info("End insertNewDriver");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            databaseService.rollbackTransaction();
+        }
+    }
 }
