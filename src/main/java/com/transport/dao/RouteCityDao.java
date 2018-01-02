000424a6e8bb35cd42807b4f489e3c90609d7f42
Paweł Hadam <pawel.hadam19@gmail.com>
2018-01-02 15:00:50 +0100

    Add insert

--- src/main/java/com/transport/dao/RouteCityDao.java
+++ src/main/java/com/transport/dao/RouteCityDao.java
@@ -1,12 +1,40 @@
 package com.transport.dao;
 
 import com.transport.DatabaseService;
+import com.transport.entity.RouteCityEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Log4j
 @RequiredArgsConstructor
 public class RouteCityDao {
     private DatabaseService databaseService = null;
 
+    private String insertNewRouteCity = "INSERT INTO " +
+            "transport.trasa_miasto (trasa_pk, miasto_pk) " +
+            "VALUES (?, ?);";
+
+    public void isnertRouteCity(RouteCityEntity newEntity) throws SQLException {
+        databaseService.setAutoCommit(false);
+        executeInsert(newEntity);
+        databaseService.setAutoCommit(true);
+    }
+
+    private void executeInsert(RouteCityEntity newEntity) {
+        try (PreparedStatement preparedStatement = databaseService.getConnection().prepareStatement(insertNewRouteCity)) {
+            log.info("Begin insertNewRouteCity");
+
+            preparedStatement.setInt(1, newEntity.getRouteId());
+            preparedStatement.setInt(2, newEntity.getCityId());
+            preparedStatement.executeUpdate();
+
+            log.info("End insertNewRouteCIty");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            databaseService.rollbackTransaction();
+        }
+    }
 }
