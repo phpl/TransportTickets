2865b40001e658750578f13a050c50aebd3d177d
Paweł Hadam <pawel.hadam19@gmail.com>
2018-01-02 14:52:41 +0100

    Add isnert

--- src/main/java/com/transport/dao/LuggageDao.java
+++ src/main/java/com/transport/dao/LuggageDao.java
@@ -1,12 +1,41 @@
 package com.transport.dao;
 
 import com.transport.DatabaseService;
+import com.transport.entity.LuggageEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Log4j
 @RequiredArgsConstructor
 public class LuggageDao {
     private DatabaseService databaseService = null;
 
+    private String insertNewDriver = "INSERT INTO " +
+            "transport.bagaz (bilet_pk, waga, uzytkownik_pk) " +
+            "VALUES (?, ?, ?);";
+
+    public void insertDriver(LuggageEntity newEntity) throws SQLException {
+        databaseService.setAutoCommit(false);
+        executeInsert(newEntity);
+        databaseService.setAutoCommit(true);
+    }
+
+    private void executeInsert(LuggageEntity newEntity) {
+        try (PreparedStatement preparedStatement = databaseService.getConnection().prepareStatement(insertNewDriver)) {
+            log.info("Begin insertNewLuggage");
+
+            preparedStatement.setInt(1, newEntity.getTicketId());
+            preparedStatement.setDouble(2, newEntity.getWeight());
+            preparedStatement.setInt(3, newEntity.getUserId());
+            preparedStatement.executeUpdate();
+
+            log.info("End insertNewLuggage");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            databaseService.rollbackTransaction();
+        }
+    }
 }
