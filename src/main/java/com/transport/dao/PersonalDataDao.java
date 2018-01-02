e48f64674829a0a3eb55b805895dd47bea7270f7
Paweł Hadam <pawel.hadam19@gmail.com>
2018-01-02 14:56:58 +0100

    Change function mame

--- src/main/java/com/transport/dao/PersonalDataDao.java
+++ src/main/java/com/transport/dao/PersonalDataDao.java
@@ -1,12 +1,43 @@
 package com.transport.dao;
 
 import com.transport.DatabaseService;
+import com.transport.entity.PersonalDataEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Log4j
 @RequiredArgsConstructor
 public class PersonalDataDao {
     private DatabaseService databaseService = null;
 
+    private String insertNewPersonalData = "INSERT INTO " +
+            "transport.dane_osobowe (uzytkownik_pk, imie, nazwisko, numer_telefonu, adres_pk) " +
+            "VALUES (?, ?, ?, ?, ?);";
+
+    public void insertPersonalData(PersonalDataEntity newEntity) throws SQLException {
+        databaseService.setAutoCommit(false);
+        executeInsert(newEntity);
+        databaseService.setAutoCommit(true);
+    }
+
+    private void executeInsert(PersonalDataEntity newEntity) {
+        try (PreparedStatement preparedStatement = databaseService.getConnection().prepareStatement(insertNewPersonalData)) {
+            log.info("Begin insertNewPersonalData");
+
+            preparedStatement.setInt(1, newEntity.getUserId());
+            preparedStatement.setString(2, newEntity.getFirstName());
+            preparedStatement.setString(3, newEntity.getLastName());
+            preparedStatement.setInt(4, newEntity.getPhoneNumber());
+            preparedStatement.setInt(5, newEntity.getAddresId());
+            preparedStatement.executeUpdate();
+
+            log.info("End insertNewPersonalData");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            databaseService.rollbackTransaction();
+        }
+    }
 }
